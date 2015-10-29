94cba4248fa1a539acfd317d2f8dad24fcf7304e
U10111033 <U10111033@go.utaipei.edu.tw>
2015-10-29 22:30:30 +0800

    Ver.1

---
+++ KeyEventDemo.java
@@ -0,0 +1,64 @@
+/**U10111033, Computer science 4, Hsueh_Hsin Lu*/
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class KeyEventDemo extends JFrame {
+	private KeyboardPanel keyboardPanel = new KeyboardPanel();
+	
+	/** Initialize UI */
+	public KeyEventDemo() {
+		// Add the keyboard panel to accept and display user input
+		add(keyboardPanel);
+
+		// Set focus
+		keyboardPanel.setFocusable(true);
+	}
+
+	/** Main method */
+	public static void main(String[] args) {
+		KeyEventDemo frame = new KeyEventDemo();
+		frame.setTitle("KeyEventDemo");
+		frame.setSize(300, 100);
+		frame.setLocationRelativeTo(null); // Center the frame   
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	// Inner class: KeyboardPanel for receiving key input
+	static class KeyboardPanel extends JPanel {
+		private int x = 10;
+		private int y = 35;
+		private char keyChar = 'A'; // Default key
+		private StringBuffer buf = new StringBuffer();
+
+		public KeyboardPanel() {
+			addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e){
+					if(e.getKeyCode() == KeyEvent.VK_ENTER){
+						//clean the StringBuffer if press ENTER key
+						buf.delete(0, buf.length());
+					}else{
+						//get the char, and append the char to the StringBuffer
+						keyChar = e.getKeyChar();
+						buf.append(keyChar);
+					}
+					
+					//repaint
+					repaint();
+				}
+			});
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			//draw it begin frme 10,35
+			g.drawString(buf.toString() , x, y);
+		}
+	}
+}
